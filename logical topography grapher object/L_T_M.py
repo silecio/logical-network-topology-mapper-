fed24b25afd796242be0952cee5df6e8745a1f2f
silecio <64319824+silecio@users.noreply.github.com>
2020-05-09 00:04:54 +1000

    Add files via upload

--- logical topography grapher object/L_T_M.py
+++ logical topography grapher object/L_T_M.py
@@ -1,7 +1,6 @@
 #object oriented ver
 from tkinter import *
 import yaml
-
 main_window = Tk()
 ##main_window.geometry("500x100")
 Value_frame = LabelFrame(main_window)
@@ -17,11 +16,14 @@ scrollbar_s = Scrollbar(C_frame, orient = HORIZONTAL)
 parents = []
 children = []
 connections = []
+M_parents =[]
+Names = []
 def clear_selec():
     parents.clear()
     children.clear()
 def add():
-    current_node = [clicked.get() , name.get()]
+    current_node = (clicked.get() , name.get())
+    print(current_node)
     #change into vairiable list
     node_list1.insert(END, current_node)
     node_list2.insert(END, current_node)
@@ -31,27 +33,35 @@ def set_parent():
     index= node_list1.curselection()
     print(index)
     parent = [x[i] for i in index]
+    print(parent)
     parents.append(parent)
+    Names.append(parents[0])
     print(parents)
 def set_child():
     x = node_list2.get(0,END)
     index= node_list2.curselection()
     child = [x[i] for i in index]
     children.append(child)
-    print(children)
-    
+
+#Working(not finished, needs to store the Yaml data and format yaml correctly) 
 def connection_button():
     #want format [[PARENT,[CHILD,CHILD...],[PARENT[CHILD,CHILD...]ETC]
-    temp = [{
-        "parent":parents[0],
-        "children":children
-    }]
-    #python to yaml
-    cons = yaml.dump(temp)
-    # yaml to python
-    cons_l = yaml.load(cons)
-    conn_list.insert(END, cons_l)
-    print(cons_l)
+    class parent:
+        def __init__(self):
+            self.name = parents[0]
+            self.cons_l = children[0]
+    
+    p = parent()
+    fin_p = {"temp":{"parent": p.name, "connections": p.cons_l}}
+    M_parents.append(fin_p)
+    Y_mp = yaml.dump(M_parents)
+    print(Y_mp)
+    print(M_parents)
+    print(Names)
+    x = p.name
+    y = ' '.join(x[0])
+
+    conn_list1.insert(END,y)
     parents.clear()
     children.clear()
     
@@ -69,9 +79,10 @@ Connec_L = Label(C_frame, text = "Connections")
 
 node_list1 = Listbox(N_frame1)
 node_list2 = Listbox(N_frame3,selectmode = 'multiple')
-conn_list = Listbox(C_frame, width = 85, xscrollcommand = scrollbar_s.set)
+conn_list1 = Listbox(C_frame)
+conn_list2 = Listbox(C_frame)
 
-scrollbar_s.config(command =  conn_list.xview)
+
 
 make_conn = Button(N_frame2_5, text = "make connection", command = connection_button)
 set_parent = Button(N_frame2, text = "set host", command = set_parent)
@@ -87,8 +98,8 @@ set_child.pack(side = RIGHT, padx = 5)
 clear_all.pack(side = BOTTOM,padx = 5, pady = 5)
 add_N.pack()
 node_list2.pack(side = RIGHT,padx = 5, anchor = 'e')
-conn_list.pack(side = BOTTOM)
-scrollbar_s.pack(side = BOTTOM,fill = X)
+conn_list1.pack(side = LEFT)
+conn_list2.pack(side = RIGHT)
 name.pack(anchor = 'n')
 Value_frame.pack(anchor = 'n')
 ##Conn_frame.pack(anchor = 'se')
